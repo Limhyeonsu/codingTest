bfb856da879bf22c5a02ca361b11333b82f0909a
Limhyeonsu <dlagustn720@naver.com>
2024-08-16 18:27:29 +0900

    [Bronze III] Title: 최댓값, Time: 168 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -4,18 +4,19 @@ public class Main
 {
 	public static void main(String[] args)  {
 		Scanner scanner = new Scanner(System.in);
-		List<Integer> list = new ArrayList<>();
-		for(int i=0; i<9; i++) {
-		    list.add(scanner.nextInt());
-		}
-		
 		int max = Integer.MIN_VALUE;
-		for(int x : list) {
-		    max = Math.max(x, max);
+		int pos= 0;
+		for(int i=0; i<9; i++) {
+		    int value = scanner.nextInt();
+		    if(value > max) {
+		        max = value;
+		        pos = i+1;
+		    }
+		    
 		}
 		
 		System.out.println(max);
-		System.out.println(list.indexOf(max)+1);
+		System.out.println(pos);
       
 	}
 }
