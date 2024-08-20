37af3058f5eaf7585781c187e9b53217bfd17671
Limhyeonsu <dlagustn720@naver.com>
2024-08-20 15:40:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args)  {
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		int answer = s.charAt(0);
+		System.out.println(answer);
+		
+	}
+}
