f3b740bb0b6e0a3fd18fb75b8987e7bb507198af
Limhyeonsu <dlagustn720@naver.com>
2024-09-24 15:39:16 +0900

    [Silver III] Title: 주유소, Time: 864 ms, Memory: 169996 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -2,50 +2,27 @@ import java.util.*;
 
 public class Main
 {
-    public static int[] price;
 	public static void main(String[] args)  {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		int[] streets = new int[n - 1];
-		price = new int[n];
+		int[] price = new int[n];
 		for(int i=0; i<n-1; i++) {
 		    streets[i] = scanner.nextInt();
 		}
-		for(int i=0; i<n; i++) {
-		    price[i] = scanner.nextInt();
+		price[0] = scanner.nextInt();
+		for(int i=1; i<n-1; i++) {
+		    int v = scanner.nextInt();
+		    if(price[i - 1] < v) price[i] = price[i - 1];
+		    else price[i] = v;
 		}
+		price[n - 1] = scanner.nextInt();
 		
-		int answer = price[0] * streets[0];
-		int nextIndex = getNextIndex(0, n);
-		if(nextIndex == 0) {
-		    for(int x=1; x<streets.length; x++) {
-		        answer += price[0] * streets[x];
-		    }
-		} else {
-		
-    		for(int x=1; x<nextIndex; x++) {
-    		    answer += price[0] * streets[x];
-    		}
-    		while(nextIndex < n-1){
-    		    int tmp = nextIndex;
-    		    nextIndex = getNextIndex(nextIndex, n);
-    		    for(int x=tmp; x<nextIndex; x++) {
-    		    answer += price[tmp] * streets[x];
-    		    }
-    		}
+		int answer = 0;
+		for(int i=0; i<n-1; i++) {
+		    answer += price[i] * streets[i];
 		}
-        System.out.println(answer);
-	}
-	
-	public static int getNextIndex(int curIndex, int n) {
-	    
-	    for(int j=curIndex + 1; j<n; j++) {
-		    if(price[curIndex] > price[j]) {
-		        return j;
-		    }
-		}
-		return curIndex;
+		System.out.println(answer);
 		
-	    
 	}
 }
